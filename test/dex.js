3a2da45780c6c92db360a137a93dcb666e7714b5
aditya-gore <aditya_gore@hotmail.com>
2021-02-02 13:13:32 +0530

    Deployed the dex smart contract for testing

--- test/dex.js
+++ test/dex.js
@@ -4,6 +4,14 @@ const Bat = artifacts.require("mocks/Bat.sol");
 const Rep = artifacts.require("mocks/Rep.sol");
 const Zrx = artifacts.require("mocks/Zrx.sol");
 
+// Import contract abstraction of the Decentralized Exchange
+const Dex = artifacts.require("Dex.sol");
+
+// Create byte32 variables using web3 for ticker argument
+const [DAI, BAT, REP, ZRX] = ["DAI", "BAT", "REP", "ZRX"].map((ticker) =>
+  web3.utils.fromAscii(ticker)
+);
+
 // Define cotract block to write tests.
 contract("Dex", () => {
   // Variables pointing to ERC20 tokens.
@@ -17,6 +25,14 @@ contract("Dex", () => {
       Bat.new(),
       Rep.new(),
       Zrx.new(),
+    ]);
+    const dex = await Dex.new(); // Deploy Dex smart contract.
+    // Configure ERC20 tokens in the Dex (Call addToken for each token)
+    await Promise.all([
+      dex.addToken(DAI, dai.address),
+      dex.addToken(BAT, bat.address),
+      dex.addToken(REP, rep.address),
+      dex.addToken(ZRX, zrx.address),
     ]);
   });
 });
